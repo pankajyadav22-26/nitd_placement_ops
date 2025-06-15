9c80e5aa4d8b3c96ea14f2741271ee20a397c06d
pankajyadav22-26 <142982981+pankajyadav22-26@users.noreply.github.com>
2025-06-15 23:47:48 +0530

    get offer route fixed

--- app/api/admin/manageOffer/get/[roll_no]/route.ts
+++ app/api/admin/manageOffer/get/[roll_no]/route.ts
@@ -1,19 +1,18 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import OfferModel from "@/models/offer";
 import { connectToDatabase } from "@/lib/db";
 
-export async function GET(
-  _req: Request,
-  { params }: { params: { roll_no: string } }
-) {
+export async function GET(req: NextRequest) {
   await connectToDatabase();
 
-  const { roll_no } = params;
+  const url = new URL(req.url);
+  const segments = url.pathname.split("/");
+  const roll_no = segments[segments.length - 1];
 
   try {
     const student = await OfferModel.findOne({ roll_no });
     if (!student) {
-      return NextResponse.json({ error: "There are no offers of this Student" }, { status: 404 });
+      return NextResponse.json({ error: "There are no offers of this student" }, { status: 404 });
     }
 
     return NextResponse.json({ data: student });
